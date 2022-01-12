a6326cad185ee1ae0654d7075353ab269e3554b2
Elton M <eltonmarku@gmail.com>
2022-01-12 22:13:12 +0100

    update meta

--- meta.ts
+++ meta.ts
@@ -1,13 +1,12 @@
 import { ResumeriseMeta } from "resumerise_library/mod.ts";
 
 export const meta = {
-  colors: ["#000", "#fff"],
-  description: "A starter template for resumerise.",
-  name: "resumerise_theme_retro",
+  colors: ["#0C77EB", "#000000", "#494744"],
+  description:
+    "Elanc is an animated and techy resume template. It is a fully responsive template with a clean and elegant design. It is very easy to customize.",
   platforms: ["DESKTOP", "INTERACTIVE", "MOBILE", "PRINT"],
-  tags: ["starter", "blueprint", "retro"],
-  categories: ["EDUCATION"],
-  styles: ["CLEAN", "MINIMAL"],
+  categories: ["ACTOR", "ARTIST", "ART", "BEAUTY", "PERSONAL"],
+  styles: ["CLEAN", "MINIMAL", "ANIMATED", "GEOMETRIC", "TECH"],
   author: "resumerise",
   authorWebUrl: "http://resumerise.io",
 } as ResumeriseMeta;
